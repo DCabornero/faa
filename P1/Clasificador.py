2549efcc14c6666c42228e2f975b1464f71766f2
DCabornero <dcabornero@gmail.com>
2020-10-19 19:15:21 +0200

    Añadido corrector de Laplace

--- P1/Clasificador.py
+++ P1/Clasificador.py
@@ -36,7 +36,7 @@ class Clasificador:
 
     # Realiza una clasificacion utilizando una estrategia de particionado determinada
     # TODO: implementar esta funcion
-    def validacion(self,particionado,dataset,seed=None):
+    def validacion(self,particionado,dataset,seed=None,laplace=False):
         # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
         # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
         # y obtenemos el error en la particion de test i
@@ -47,7 +47,7 @@ class Clasificador:
         for i, p in enumerate(particionado.particiones):
             datostrain = dataset.extraeDatos( p.indicesTrain )
             datostest = dataset.extraeDatos( p.indicesTest )
-            self.entrenamiento( datostrain, dataset.nominalAtributos, dataset.diccionario )
+            self.entrenamiento( datostrain, dataset.nominalAtributos, dataset.diccionario, laplace=laplace )
             pred = self.clasifica( datostest, dataset.nominalAtributos, dataset.diccionario )
             errs[i] = self.error( datostest, pred )
         return errs
@@ -58,10 +58,15 @@ class Clasificador:
 
 class ClasificadorNaiveBayes(Clasificador):
 
-    def procesaDiscreto(self, arr, numX, numY):
+    def procesaDiscreto(self, arr, numX, numY, laplace):
         foo = np.zeros((numX, numY))
         for row in arr:
             foo[row[0], row[1]] += 1
+        #Corrección de Laplace
+        if laplace:
+            numCeros = np.sum(foo == 0)
+            if numCeros > 0:
+                foo += 1
         #Normalizamos cada columna
         for i in range(np.shape(foo)[1]):
             foo[:,i] /= np.sum(foo[:,i])
@@ -83,12 +88,12 @@ class ClasificadorNaiveBayes(Clasificador):
         return foo / np.sum(foo)
 
     # TODO: implementar
-    def entrenamiento(self,datostrain,atributosDiscretos,diccionario):
+    def entrenamiento(self,datostrain,atributosDiscretos,diccionario,laplace=False):
         self.entrenamientoAux = []
         for i in range(len(atributosDiscretos)-1):
             foo = np.column_stack((datostrain[:,i],datostrain[:,-1]))
             if atributosDiscretos[i]:
-                self.entrenamientoAux.append(self.procesaDiscreto(foo, len(diccionario[i]), len(diccionario[-1])))
+                self.entrenamientoAux.append(self.procesaDiscreto(foo, len(diccionario[i]), len(diccionario[-1]),laplace))
             else:
                 self.entrenamientoAux.append(self.procesaContinuo(foo, len(diccionario[-1])))
         self.entrenamientoAux.append(self.procesaFinal(datostrain[:,-1], len(diccionario[-1])))
