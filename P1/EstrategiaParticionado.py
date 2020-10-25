bf7ad0d0dc86e75f0e4393b550987f6598cfea81
DCabornero <dcabornero@gmail.com>
2020-10-25 13:08:12 +0100

    Comentado EstrategiaParticionado

--- P1/EstrategiaParticionado.py
+++ P1/EstrategiaParticionado.py
@@ -2,6 +2,8 @@ from abc import ABCMeta,abstractmethod
 import numpy as np
 import random
 
+# Clase que permite separar índices de un conjunto de datos en dos arrays: el de entrenamiento
+# y el de test.
 class Particion():
 
     # Esta clase mantiene la lista de �ndices de Train y Test para cada partici�n del conjunto de particiones
@@ -11,6 +13,8 @@ class Particion():
 
 #####################################################################################################
 
+# Clase abstacta diseñada para elaborar a partir de ella distintas estrategias que
+# permiten crear una o varias particiones de datos
 class EstrategiaParticionado:
 
     # Clase abstracta
@@ -19,15 +23,19 @@ class EstrategiaParticionado:
     # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor
 
     @abstractmethod
-    # TODO: esta funcion deben ser implementadas en cada estrategia concreta
+    # Función abstacta encargada de crear las particiones que se quieran. Devuelve una clase Particion
+    # datos: clase Datos que contiene los datos que se quieren particionar
+    # seed: semilla que permite controlar los barajeos de ejemplos
     def creaParticiones(self,datos,seed=None):
         pass
 
 
 #####################################################################################################
-
+# Estrategia de particionado que permite aplicar el método de validación simple.
 class ValidacionSimple(EstrategiaParticionado):
 
+    # proporcionTest: proporcion de datos que se quieren destinar al testSet frente al trainSet
+    # numeroEjecuciones: número de veces que se quiere particionar la base de datos
     def __init__(self, proporcionTest, numeroEjecuciones=1):
         # TODO: comprobar que esta entre 0 y 1
         self.particiones = []
@@ -35,14 +43,14 @@ class ValidacionSimple(EstrategiaParticionado):
         self.numeroEjecuciones = numeroEjecuciones
 
     # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el n�mero de ejecuciones deseado
-    # Devuelve una lista de particiones (clase Particion)
-    # TODO: implementar
+    # Añade el atributo 'particiones' a la clase. 'particiones' es un array de clases Particion.
     def creaParticiones(self,datos,seed=None):
         random.seed(seed)
         numDatos = len(datos.datos)
         indexList = [i for i in range(numDatos)]
         numTest = int(np.floor(numDatos * self.proporcionTest))
         self.particiones = []
+        # Cálculo de las 'numeroEjecuciones' particiones que se quieran crear.
         for _ in range(self.numeroEjecuciones):
             p = Particion()
             random.shuffle(indexList)
@@ -53,25 +61,28 @@ class ValidacionSimple(EstrategiaParticionado):
 
 
 #####################################################################################################
+# Estrategia de particionado que permite aplicar el método de validación cruzada.
 class ValidacionCruzada(EstrategiaParticionado):
-
+    # numeroParticiones: cantidad de particiones que se hacen a los datos.
     def __init__(self, numeroParticiones):
         self.particiones = []
         self.numeroParticiones = numeroParticiones
 
-    # Crea particiones segun el metodo de validacion cruzada.
+    # Crea particiones segun el método de validacion cruzada.
     # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
-    # Esta funcion devuelve una lista de particiones (clase Particion)
-    # TODO: implementar
+    # Esta funcion crea el atributo de la clae particiones, una lista de particiones (clase Particion)
     def creaParticiones(self,datos,seed=None):
         random.seed(seed)
         numDatos = len(datos.datos)
         indexList = [i for i in range(numDatos)]
         random.shuffle(indexList)
+        # Cálculo del tamaño de cada bloque de datos y separación en ese tamaño
         chunkSize = int(np.floor(numDatos / self.numeroParticiones))
         bloques = [ indexList[i*chunkSize:(i + 1)*chunkSize] for i in range(self.numeroParticiones - 1) ]
         bloques.append( indexList[(self.numeroParticiones - 1)*chunkSize:] )
         self.particiones = []
+        # Creación de las particiones. En cada una se utiliza un bloque como testSet y el resto como trainSet.
+        # Por ello, el número de particiones es el número de bloques.
         for i in range(len(bloques)):
             p = Particion()
             p.indicesTest = bloques[i]
