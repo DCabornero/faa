021798d7ee23973c7301b32469be6d493ee553a7
DCabornero <dcabornero@gmail.com>
2020-10-25 12:38:12 +0100

    Comentado Clasificador

--- P1/Clasificador.py
+++ P1/Clasificador.py
@@ -4,14 +4,16 @@ from scipy.stats import norm
 
 from EstrategiaParticionado import ValidacionSimple
 
+# Clase abstacta a partir de la cual se implementarán todos los clasificadores
 class Clasificador:
 
     # Clase abstracta
     __metaclass__ = ABCMeta
 
-    # Metodos abstractos que se implementan en casa clasificador concreto
+    # Metodos abstractos que se implementan en cada clasificador concreto
     @abstractmethod
-    # TODO: esta funcion debe ser implementada en cada clasificador concreto
+    # La función entrenamiento permite obtener los parámetros necesarios para
+    # realizar predicciones
     # datosTrain: matriz numpy con los datos de entrenamiento
     # atributosDiscretos: array bool con la indicatriz de los atributos nominales
     # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion de variables discretas
@@ -20,14 +22,17 @@ class Clasificador:
 
 
     @abstractmethod
-    # TODO: esta funcion debe ser implementada en cada clasificador concreto
-    # devuelve un numpy array con las predicciones
+
+    # Una vez entrenado, el clasificador devuleve un array de predicciones
+    # dado un testSet
+    # datosTest: matriz numpy con los datos de prueba
+    # atributosDiscretos: array bool con la indicatriz de los atributos nominales
+    # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion de variables discretas
     def clasifica(self,datosTest,atributosDiscretos,diccionario):
         pass
 
 
-    # Obtiene el numero de aciertos y errores para calcular la tasa de fallo
-    # TODO: implementar
+    # Obtiene el numero de aciertos y errores y devuelve la tasa de fallo
     def error(self,datos,pred):
         # Aqui se compara la prediccion (pred) con las clases reales y se calcula el error
         comp = np.column_stack((datos[:,-1],pred))
@@ -35,18 +40,21 @@ class Clasificador:
         return np.average(failsArr)
 
 
-    # Realiza una clasificacion utilizando una estrategia de particionado determinada
-    # TODO: implementar esta funcion
+    # Realiza una clasificacion utilizando una estrategia de particionado determinada.
+    # Devuelve un array con los errores de cada una de las clasificaciones hechas
     def validacion(self,particionado,dataset,clasificador=None,seed=None):
-        # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
-        # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
-        # y obtenemos el error en la particion de test i
-        # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
-        # y obtenemos el error en la particion test. Otra opci�n es repetir la validaci�n simple un n�mero especificado de veces, obteniendo en cada una un error. Finalmente se calcular�a la media.
+        # Se permite la creación de una clase abstracta Clasificador, de forma que mediante el parámetro clasificador
+        # se introduzca la implementación de los métodos 'entrenamiento' y 'clasifica'. Se ha decidido realizar esta implementación
+        # porque se incluía el parámetro clasificador en la plantilla. A priori, asumimos que estamos trabajando con una clase
+        # que ya ha instanciado estas funciones.
         if clasificador == None:
             clasificador = self
+        # Obtenemos las particiones según la estrategia de particionado dada
         particionado.creaParticiones(dataset,seed=seed)
         errs = np.zeros(len(particionado.particiones))
+        # Para cada una de las particiones entrenamos al clasificador con el trainSet
+        # y clasificamos los ejemplos del testSet a la vez que calculamos la tasa de
+        # acierto de esta clasificación
         for i, p in enumerate(particionado.particiones):
             datostrain = dataset.extraeDatos( p.indicesTrain )
             datostest = dataset.extraeDatos( p.indicesTest )
@@ -55,6 +63,10 @@ class Clasificador:
             errs[i] = self.error( datostest, pred )
         return errs
 
+    # Función que, dados una matriz de datos en formato numpy y sus predicciones
+    # calcula la matriz de confusión correspondiente. Si no queda claro cual es la
+    # clase positiva y cual es la negativa, la selección de falsos positivos y
+    # falsos negativos es arbitraria.
     def confusion_matrix(self,datos,pred):
         conf_mat = np.zeros((2,2))
         comp = np.column_stack((datos[:,-1],pred))
@@ -63,12 +75,16 @@ class Clasificador:
             conf_mat[foo[(x[0] == 1, x[1] == 1)]] += 1
         return conf_mat
 
+    # Función que dada una clase Datos y la proporcion de testSet nos devuelve la
+    # matriz de confusión obtenida mediante validación simple.
     def get_confusion_matrix(self,dataset,seed=None,proporcionTest=0.3):
+        # Creación de la única partición
         particionado = ValidacionSimple(proporcionTest)
         particionado.creaParticiones(dataset,seed=seed)
         p = particionado.particiones[0]
         datostrain = dataset.extraeDatos( p.indicesTrain )
         datostest = dataset.extraeDatos( p.indicesTest )
+        # Entrenamiento y clasificación de los datos dados
         self.entrenamiento( datostrain, dataset.nominalAtributos, dataset.diccionario )
         pred = self.clasifica( datostest, dataset.nominalAtributos, dataset.diccionario )
         return self.confusion_matrix( datostest, pred )
@@ -76,14 +92,29 @@ class Clasificador:
 ##############################################################################
 
 
-
+# Clase que hereda el método abstracto clasificador. Entrena y clasifica conjuntos
+# de datos siguiendo la estrategia Naive-Bayes.
 class ClasificadorNaiveBayes(Clasificador):
 
+    # El parámetro opcional laplace permite que se aplique la corrección de Lapace
+    # al conjunto de entrenamiento si fuera necesario.
     def __init__(self, laplace=False):
         self.laplace = laplace
 
+    # Obtenemos a partir de una simplificación de la matriz de datos la proporcion
+    # de que aparezca un cierto atributo discreto dada cada clase.
+    # arr: array de duplas donde cada dupla corresponde a un ejemplo. De ese ejemplo
+    # el primer dato de la dupla se corresponde con el valor que tiene un cierto atributo
+    # y el segundo con la clase del ejemplo
+    # numX: número de valores que adopta el atributo
+    # numY: número de clases
+    # laplace: booleano que indica si hay que realizar la corrección de Laplace
+    # Se devuelve una matriz donde cada fila corresponde a un valor de un atributo
+    # y cada columna corresponde a una clase. Como estamos mostrando la proporción
+    # de datos relativos a cada clase, los valores de las columnas deben sumar 1.
     def procesaDiscreto(self, arr, numX, numY, laplace):
         foo = np.zeros((numX, numY))
+        # Tabla de frecuencias
         for row in arr:
             foo[row[0], row[1]] += 1
         #Corrección de Laplace
@@ -96,6 +127,10 @@ class ClasificadorNaiveBayes(Clasificador):
             foo[:,i] /= np.sum(foo[:,i])
         return foo
 
+    # Función con las mismas características que la función anterior, pero ahora el
+    # atributo toma valores continuos. La función devuelve un array de duplas de dimensión
+    # el número de clases, donde cada dupla representa la media y la desviación típica
+    # de los datos del atributo relativos a una cierta clase.
     def procesaContinuo(self, arr, numY):
         means = np.zeros(numY)
         stds = np.zeros(numY)
@@ -105,26 +140,33 @@ class ClasificadorNaiveBayes(Clasificador):
             stds[i] = np.std(foo)
         return (means, stds)
 
+    # Cálculo de priores dada la columna de clases del dataset.
     def procesaFinal(self, arr, numY):
         foo = np.zeros(numY)
         for row in arr:
             foo[row] += 1
         return foo / np.sum(foo)
 
-    # TODO: implementar
+    # Entrenamiento realizado mediante la estrategia Naive-Bayes.
     def entrenamiento(self,datostrain,atributosDiscretos,diccionario):
         self.entrenamientoAux = []
+        # Para cada atributo, obtenemos los datos necesarios.
         for i in range(len(atributosDiscretos)-1):
             foo = np.column_stack((datostrain[:,i],datostrain[:,-1]))
+            # Si el atributo es discreto, obtenemos la matriz de ocurrencias normalizada por clases.
             if atributosDiscretos[i]:
                 self.entrenamientoAux.append(self.procesaDiscreto(foo, len(diccionario[i]), len(diccionario[-1]),self.laplace))
+            # Si el atributo es continuo, obtenemos la media y desviación típica del atributo
+            # para cada clase.
             else:
                 self.entrenamientoAux.append(self.procesaContinuo(foo, len(diccionario[-1])))
+        # Obtenemos los priores
         self.entrenamientoAux.append(self.procesaFinal(datostrain[:,-1], len(diccionario[-1])))
 
 
 
-    # TODO: implementar
+    # Clasificación de un cierto testSet según Naive-Bayes una vez obtenidos los datos
+    # de entrenamiento.
     def clasifica(self,datostest,atributosDiscretos,diccionario):
         numRows = np.shape(datostest)[0]
         numCols = len(atributosDiscretos)
@@ -134,9 +176,11 @@ class ClasificadorNaiveBayes(Clasificador):
             preds = np.ones(len(diccionario[-1]))
             # Hallamos la probabilidad de cada atributo independiente
             for j in range(numCols-1):
+                # Caso discreto
                 if atributosDiscretos[j]:
                     atrMatrix = self.entrenamientoAux[j]
                     preds = np.multiply(atrMatrix[datostest[i,j],:],preds)
+                # Caso continuo
                 else:
                     probs = []
                     means, stds = self.entrenamientoAux[j]
@@ -144,5 +188,6 @@ class ClasificadorNaiveBayes(Clasificador):
                     preds = np.multiply(probs, preds)
             # Hallamos la probabilidad del prior
             preds = np.multiply(self.entrenamientoAux[-1],preds)
+            # Elección de la clase con mejor probabilidad a posteriori
             clasif[i] = np.argmax(preds)
         return clasif
